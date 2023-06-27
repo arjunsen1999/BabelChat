ac641a185f71fbc400ee67f175f98a112b41edbb
arjunsen1999 <arjunsen717@gmail.com>
2023-06-27 13:24:15 +0530

    added user auth model

---
+++ server/models/auth.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const authSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    email: { type: String, unique: true, required: true, trim: true },
+    password: { type: String, required: true, trim: true },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+const AuthModel = mongoose.model("auth", authSchema);
+module.exports = { AuthModel };
