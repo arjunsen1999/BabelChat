0d965b5a1f776c54d76412e7dc466b04aba77b8b
arjunsen1999 <arjunsen717@gmail.com>
2023-06-27 15:24:24 +0530

    update auth model schema

--- server/models/auth.model.js
+++ server/models/auth.model.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const authSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
+    profile_picture: { type: String, trim: true },
     email: { type: String, unique: true, required: true, trim: true },
     password: { type: String, required: true, trim: true },
   },
