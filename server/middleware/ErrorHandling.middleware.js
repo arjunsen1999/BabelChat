2c937d0d942bb5268209002021261ad6b871dbc6
arjunsen1999 <arjunsen717@gmail.com>
2023-06-27 15:21:24 +0530

    update error handling

--- server/middleware/ErrorHandling.middleware.js
+++ server/middleware/ErrorHandling.middleware.js
@@ -1,8 +1,7 @@
 const ErrorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode ? err.statusCode : 500;
 
-  res.status(statusCode);
-  res.json({
+  return res.status(statusCode).json({
     status: err.status,
     message: err.message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,4 +1,8 @@
 const authRouter = require("express").Router();
+const auth = require("../controller/auth.controller");
+
+authRouter.route("/signup").post(auth.signup);
+authRouter.route("/login").post(auth.login);
 
 module.exports = {
   authRouter,
