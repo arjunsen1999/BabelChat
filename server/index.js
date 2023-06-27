35caab244d442dd5da041c932c7e87c2b865610a
arjunsen1999 <arjunsen717@gmail.com>
2023-06-27 13:03:54 +0530

    added connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT;
+const { connection } = require("./config/db");
+
+//All Routes import here
 
 // Global Middleware
 app.use(express.json());
@@ -10,14 +13,12 @@ app.use(cors());
 
 // All Routes
 
-
 // if Routes are not exists
-
 
 // Error handling middleware
 
-
 // listening server and connection
-app.listen(PORT, () =>{
-    console.log({server : `http://localhost:${PORT}`});
-})
+app.listen(PORT, () => {
+    connection();
+  console.log({ server: `http://localhost:${PORT}` });
+});
