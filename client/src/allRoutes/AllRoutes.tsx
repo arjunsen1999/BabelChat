371cc7341366119e282705cf0a77deb93dbcc1c8
arjunsen1999 <arjunsen717@gmail.com>
2023-06-27 16:17:57 +0530

    added login and signup page file

--- client/src/allRoutes/AllRoutes.tsx
+++ client/src/allRoutes/AllRoutes.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import ErrorPage from "../pages/404/ErrorPage";
+import LoginPage from "../pages/Login/LoginPage";
+import SignUpPage from "../pages/SignUp/SignUpPage";
 
 export default function AllRoutes() {
   return (
     <>
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
         <Route path="/" element={<ErrorPage />} />
       </Routes>
     </>
