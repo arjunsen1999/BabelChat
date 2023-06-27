c7fd2573b5c4d3c5f86512ccefd74299bd6bd0b0
arjunsen1999 <arjunsen717@gmail.com>
2023-06-27 15:15:44 +0530

    added user login backend

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -2,7 +2,9 @@ const asyncHandler = require("express-async-handler");
 const { AuthModel } = require("../models/auth.model");
 const { AppError } = require("../class/AppError");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
+// Signup :-> http://localhost:8080/auth/signup
 exports.signup = asyncHandler(async (req, res, next) => {
   const { name, email, password } = req.body;
 
@@ -24,5 +26,38 @@ exports.signup = asyncHandler(async (req, res, next) => {
     message: "Signup Successfully!",
   };
 
+  return res.status(200).json(response);
+});
+
+// Login :-> http://localhost:8080/auth/login
+
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // if user is not exists throw error
+  const isUserExists = await AuthModel.findOne({ email });
+  if (!isUserExists) {
+    next(new AppError(`You have to Signup first`, 404));
+  }
+  // if user exists then check password
+  const isPassword = await bcrypt.compare(password, isUserExists.password);
+  if (!isPassword) {
+    next(new AppError(`Invalid credentials`, 400));
+  }
+
+  // if password is match then create jwt token
+  const token = jwt.sign({ _id: isUserExists._id }, process.env.SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES,
+  });
+
+  // send success response
+  const response = {
+    status: "success",
+    message: "Successfully Login",
+    data: {
+      token,
+      name: isUserExists.name,
+    },
+  };
   return res.status(200).json(response);
 });
