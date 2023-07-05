929d87a4cb12403b5529bf4b55834ff4f1db3e3a
arjunsen1999 <arjunsen717@gmail.com>
2023-07-05 21:06:16 +0530

    update home page

---
+++ client/src/components/home/RecentContainer.tsx
@@ -0,0 +1,13 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+import PageHeading from '../heading/PageHeading'
+
+export default function RecentContainer() {
+  return (
+    <>
+    <Box w="100%" h="100%" border={"1px solid red"}>
+        <PageHeading heading='Recent'/>
+    </Box>
+    </>
+  )
+}
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { Box } from "@chakra-ui/react";
 import React from "react";
 import SideBar from "../../components/sidebar/SideBar";
+import RecentContainer from "../../components/home/RecentContainer";
 
 export default function Home() {
   return (
@@ -13,7 +14,16 @@ export default function Home() {
         gridTemplateColumns={"120px 1fr"}
       >
         <SideBar />
-        <Box border={"1px solid green"}>{/* /// Code Here */}</Box>
+        <Box
+          border={"1px solid green"}
+          display={"grid"}
+          gridTemplateColumns={"300px 1fr"}
+        >
+          <Box h='100vh' p={"40px 20px"} borderRightWidth={"1px"}>
+            <RecentContainer />
+          </Box>
+          <Box h='100vh' p={"40px 20px"}></Box>
+        </Box>
       </Box>
     </>
   );
