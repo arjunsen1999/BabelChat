9ce0af52e86a3930c0153e1392ef7cdeeafc2cae
arjunsen1999 <arjunsen717@gmail.com>
2023-07-07 13:35:33 +0530

    added vaidation

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,30 @@
 const authRouter = require("express").Router();
 const auth = require("../controller/auth.controller");
+const middleware = require("../middleware/ValidationError.middleware");
+const { body } = require("express-validator");
 
-authRouter.route("/signup").post(auth.signup);
-authRouter.route("/login").post(auth.login);
+authRouter.route("/signup").post(
+  [
+    body("name", "Enter a vaild email").isEmail(),
+    body("email", "Enter a vaild email").isEmail(),
+    body("password", "Password length must be atleast 4").isLength({
+      min: 4,
+    }),
+  ],
+  middleware.validationError,
+  auth.signup
+);
+
+authRouter.route("/login").post(
+  [
+    body("email", "Enter a vaild email").isEmail(),
+    body("password", "Password length must be atleast 4").isLength({
+      min: 4,
+    }),
+  ],
+  middleware.validationError,
+  auth.login
+);
 
 module.exports = {
   authRouter,
